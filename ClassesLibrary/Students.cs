c6d4ab198f212c411b5567f0f1e7da9567671c2a
Alexis Woodward <alexis.woodward1717@outlook.com>
2020-10-06 18:13:08 -0500

    completed Students branch

---
+++ ClassesLibrary/Students.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Students
+    {
+        //field
+        private string _firstName;
+        private string _lastName;
+        private string _id;
+        private float _gpa;
+
+        //properties
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }//end FirstName
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }//end LastName
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }//end Id
+        public float Gpa
+        {
+            get { return _gpa; }
+            set { _gpa = value; }
+        }//end Gpa
+
+        
+
+        //constructor
+        public Students(string firstName, string lastName, string id, float gpa)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id;
+            Gpa = gpa;
+        }//end constructors
+
+        //default CTOR
+        public Students() { }
+
+        //methods
+        public override string ToString()
+        {
+            return string.Format("{0} {1}\n\nID: {2}\nGPA: {3:n2}\n", FirstName, LastName, Id, Gpa);
+        }
+    }
+}
---
+++ TesterProgram/TesterProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Reference the ClassesLibrary
+using ClassesLibrary;
+
+namespace TesterProgram
+{
+    class TesterProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CSF2 Homework\n\n");
+
+            Students s1 = new Students();
+            s1.FirstName = "Allison";
+            s1.LastName = "Jackson";
+            s1.Id = "7784565";
+            s1.Gpa = 3.67f;
+
+            Console.WriteLine(s1);
+        }
+    }
+}
