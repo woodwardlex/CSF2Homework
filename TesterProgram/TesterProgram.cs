f647190b5b1810727646e570947ed3646e8363fc
Alexis Woodward <alexis.woodward1717@outlook.com>
2020-10-07 17:29:34 -0500

    Completed ContactInfo branch

--- TesterProgram/TesterProgram.cs
+++ TesterProgram/TesterProgram.cs
@@ -13,6 +13,8 @@ namespace TesterProgram
         static void Main(string[] args)
         {
             Console.WriteLine("CSF2 Homework\n\n");
+
+            #region Students
             Console.WriteLine("Students\n");
 
             Students s1 = new Students();
@@ -22,13 +24,15 @@ namespace TesterProgram
             s1.Gpa = 3.67f;
 
             Console.WriteLine(s1);
+            Console.WriteLine();
 
             Students s2 = new Students("Rodger", "Williamson", "8895655", 2.89f);
 
             Console.WriteLine(s2);
-
             Console.WriteLine();
+            #endregion
 
+            #region Vehicles
             Console.WriteLine("Vehicles\n");
 
             Vehicles v1 = new Vehicles();
@@ -44,7 +48,9 @@ namespace TesterProgram
             Console.WriteLine(v2);
 
             Console.WriteLine();
-
+            #endregion
+            
+            #region Login
             Console.WriteLine("Login\n");
 
             Login l1 = new Login();
@@ -52,12 +58,32 @@ namespace TesterProgram
             l1.Password = "ilovepineapples12";
 
             Console.WriteLine(l1);
+            Console.WriteLine();
 
             Login l2 = new Login("tjmaxx", "shoesshoesshoes");
 
             Console.WriteLine(l2);
-
             Console.WriteLine();
+            #endregion
+
+            #region Contact Info
+            Console.WriteLine("Contact Information\n");
+
+            ContactInfo p1 = new ContactInfo();
+            p1.StreetAddress = "32326 Burberry Lane";
+            p1.City = "Los Angeles";
+            p1.State = "California";
+            p1.Zip = "90016";
+            p1.Phone = "(901)465-5562";
+            p1.Email = "karatekid12@hotmail.com";
+
+            Console.WriteLine(p1);
+
+            ContactInfo p2 = new ContactInfo("99900 Runaway Blvd", "Transylvania", "Pennsylvania", "66600", "(636)222-6660", "itsspookyseason@aol.com");
+
+            Console.WriteLine(p2);
+
+            #endregion
 
 
         }//end main
